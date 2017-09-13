86363ff3099e681e192e5e3dd71402fe29caa035
Adam Fudge <coolfudge@gmail.com>
2017-09-13 19:15:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
 class Person
+
   def name
     @name
   end
-
   def name=(new_name)
     @name = new_name
   end
@@ -10,8 +10,8 @@ class Person
   def job
     @job
   end
-
   def job=(job)
     @job = job
   end
+
 end
